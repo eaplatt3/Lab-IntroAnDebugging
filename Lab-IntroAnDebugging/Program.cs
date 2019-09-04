4c56d7f1b6542ab158468ce672fa86586d4bfacf
Earl Platt III <eaplatt3@gmail.com>
2019-09-04 12:01:31 -0400

    Add project files.

---
+++ Lab-IntroAnDebugging/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_IntroAnDebugging
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            double dBill;
+            int iTipPrecent;
+            double dTipConversion;
+            double dTotal;
+            string strI;
+
+            Console.Write("Enter Bill Amount: ");
+            strI = Console.ReadLine();
+
+            if (!double.TryParse(strI, out dBill))
+            {
+
+            }
+
+            Console.Write("Enter Tip Percentage: ");
+            strI = Console.ReadLine();
+
+            if (!Int32.TryParse(strI, out iTipPrecent))
+            {
+
+            }
+
+            dTipConversion = iTipPrecent % 100;
+
+            Console.WriteLine(dTipConversion);
+
+            dTotal = dTipConversion + dBill;
+
+            Console.WriteLine("Total is: " + dTotal);
+
+
+
+
+        }
+    }
+}
